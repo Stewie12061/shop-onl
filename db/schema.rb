2992b906876eac77f3b2028799bc5548ad376d44
Stewie12061 <nhut170501@gmail.com>
2021-07-05 19:47:06 +0700

    Revert "new"
    This reverts commit 25dbda546dbe923c4ebc1e4816eb27d359fa7861.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_122333) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_06_30_133928) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
