9ee5f322bb346ef69ced966d3a6e05b88a35a988
Stewie12061 <nhut170501@gmail.com>
2021-07-25 01:25:15 +0700

    add user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def user_login?
+    !current_user.nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
   get '/contact', controller: :contact, action: :index
   get '/about', controller: :about, action: :index
   # get '/products', controller: :products, action: :index
